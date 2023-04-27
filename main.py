bace17092c3d61210682b52e52c7692725333c1c
Tim <tim@dropsoft.org>
2023-04-27 13:39:23 +0200

    Added working Version of CardBoxSystem

---
+++ main.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append("/home/tim/Desktop/.Programmieren/Python/Karteikarten/LibaryAndStorage")
+from CardBoxLib import Libary
+
+
+if __name__ == "__main__":
+    while True:
+        Libary.menu()
